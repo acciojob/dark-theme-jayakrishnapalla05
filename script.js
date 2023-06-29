a59b16a9f64c8a32ed19c9af6ed924fa69e6c63b
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-29 12:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+function swapTheme(){
+	const app=document.getElementById("app");
+	const swap=document.getElementById("swap");
+
+	if(app.classList.contains("day"))
+	{
+		app.classList.remove("day");
+		app.classList.add("night");
+
+		swap.classList.remove("button_day");
+		swap.classLsit.add("button_night");
+	}
+	else
+	{
+		app.classList.remove("night");
+		app.classList.add("day");
+
+		swap.classList.remove("button_night");
+		swap.classLsit.add("button_day");
+		
+	}
+}
+swapTheme();
 
